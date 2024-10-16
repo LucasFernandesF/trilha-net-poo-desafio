2d909104ffa849fb8637da85f152c56f21fc334d
Lucas Fernandes <lucasffb120@gmail.com>
2024-10-16 12:04:30 -0300

    Resolvendo os desafios proposto pelos desafios

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizando os testes com as classes Nokia e Iphone
+Iphone celular1 = new Iphone("3199999-9999", "IPHONE 14 PRO MAX", "123456", 128);
+
+
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("----------------------------");
+
+Iphone celular2 = new Iphone("3199999-9999", "NOKIA C21", "654321", 32);
+
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Instagram");
